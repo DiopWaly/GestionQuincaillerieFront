0a570db48f4efd63cfb5756ffc9989761e0325fa
atles <mourtadandiaye77@gmail.com>
2020-09-25 13:53:45 +0000

    creation des formulaires d'ajout de tous les entités

--- src/app/entities/operation/operation.component.ts
+++ src/app/entities/operation/operation.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { CrudService } from './../../services/crud.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OperationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private crud : CrudService,private router : Router) { }
 
   ngOnInit(): void {
+  }
+  add(f){
+    console.log(f);
+    this.crud.add("operation/add",f)
+        .subscribe(data=>{
+          console.log(data);
+          this.router.navigate(['listoperation']);
+        },err=>{
+          console.log(err);
+
+        });
   }
 
 }
--- src/app/entities/user/user.component.ts
+++ src/app/entities/user/user.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { CrudService } from './../../services/crud.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private crud : CrudService,private router : Router) { }
 
   ngOnInit(): void {
+  }
+  add(f){
+    console.log(f);
+    this.crud.add("user/add",f)
+        .subscribe(data=>{
+          console.log(data);
+          this.router.navigate(['listuser']);
+        },err=>{
+          console.log(err);
+
+        });
   }
 
 }
